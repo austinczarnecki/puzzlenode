ae2dcf6e50434a27a9b096109b6620458f818c5e
Austin Czarnecki <austin.czarnecki@gmail.com>
2013-08-06 19:43:40 +0000

    more renaming

--- [3]spellingsuggestions/spelling.rb
+++
@@ -1,8 +0,0 @@
-require './spelling_suggestions'
-require './query_case'
-
-file_name = ARGV.first
-
-spelling = SpellingSuggestions.new(file_name)
-
-spelling.spelling_suggestions
--- [5]countingcards/game.rb
+++
@@ -1,9 +0,0 @@
-class Game
-  def initialize(shady, rocky, danny, lil, discard)
-    @shady = shady
-    @rocky = rocky
-    @danny = danny
-    @lil = lil
-    @discard = discard
-  end
-end
